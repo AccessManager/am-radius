d15b54f5a69f8fb7106d7189675646f439babda9
Rahul makhija <rahulmkhj@gmail.com>
2014-08-03 17:45:48 +0530

    fixed constant conflicts

--- src/Helpers/Constants.php
+++ src/Helpers/Constants.php
@@ -1,14 +1,37 @@
 <?php
 
+if( ! defined('LIMITED') )
 define("LIMITED", 1);
+
+if( ! defined('UNLIMITED') )
 define("UNLIMITED", 0);
+
+if( ! defined('TIME_LIMIT') )
 define('TIME_LIMIT', 0);
+
+if( ! defined('DATA_LIMIT') )
 define('DATA_LIMIT', 1);
+
+if( ! defined('BOTH_LIMITS') )
 define('BOTH_LIMITS', 2);
+
+if( ! defined('NOT_ALLOWED') )
 define('NOT_ALLOWED', 0);
+
+if( ! defined('ALLOWED') )
 define('ALLOWED', 1);
+
+if( ! defined('PARTIAL') )
 define('PARTIAL', 2);
+
+if( ! defined('FOUR_GB') )
 define('FOUR_GB', 4294967296);
+
+if( ! defined('KB') )
 define('KB', 1024);
+
+if( ! defined('MB') )
 define('MB', 1048576);
+
+if( ! defined('GB') )
 define('GB', 1073741824);
