95363aa6de700d578a3c873d82716f3ec890f117
Rahul makhija <rahulmkhj@gmail.com>
2015-04-07 11:53:36 +0530

    fixed: changed require_once to require
    It was causing error when used from within access-manager project,
    instead of configuration it’ll return only TRUE as the file as already
    included.

--- src/Helpers/Database.php
+++ src/Helpers/Database.php
@@ -12,12 +12,11 @@ class Database extends Capsule{
 	public static function connect()
 	{
 		if( static::$instance === NULL ) {
-			$db = require_once __DIR__."/../../../../../app/config/database.php";
+			$db = require __DIR__."/../../../../../app/config/database.php";
 			$configdb = $db['connections']['mysql'];
 			$instance = new self;
 			$instance->addConnection($configdb);
 			$instance->setFetchMode(PDO::FETCH_CLASS);
-			$instance->setEventDispatcher(new Dispatcher( new Container ) );
 			$instance->setAsGlobal();
 		}
 		
